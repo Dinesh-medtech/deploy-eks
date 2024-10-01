43955fb3f8cd1e4475deeffb6365a7636ec51d16
Dinesh-medtech <dineshkumar@medtechsoft.co>
2024-10-01 16:22:05 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region = "us-west-2"  # Change to your preferred region
+}
+
+# Create VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create Subnet
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"  # Change to your preferred AZ
+}
+
+# Create IAM Role for EKS
+resource "aws_iam_role" "eks_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+}
+
+# Define the IAM policy for the EKS role
+data "aws_iam_policy_document" "eks_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+# Attach a policy to the IAM role
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create Security Group
+resource "aws_security_group" "eks_sg" {
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all inbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EKS Cluster
+resource "aws_eks_cluster" "example" {
+  name     = "example-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example.id]
+    security_group_ids = [aws_security_group.eks_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_policy_attachment
+  ]
+}
